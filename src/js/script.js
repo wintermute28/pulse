0ec550cb0b6dcc588266d63ae4b3734960a61b8a
wintermute28 <a28c@yandex.ru>
2023-08-03 20:45:43 +0300

    Differents carousel(under comment)

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,19 @@ $(document).ready(function(){
           ]
       });
   });
+
+// const slider = tns({
+//     container: '.carousel__inner',
+//     items: 1,
+//     slideBy: 'page',
+//     autoplay: false,
+//     controls: false,
+//   });
+
+// document.querySelector('.prev').addEventListener('click', function ()  {
+//     slider.goTo('prev');
+// });
+
+// document.querySelector('.next').addEventListener('click', function ()  {
+//     slider.goTo('next');
+// });
